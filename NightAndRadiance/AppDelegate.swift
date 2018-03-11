15a1211b6c5d5075e5e17e3093c6bdd5212b12a1
Darin Stoker <dstoker51@gmail.com>
2018-03-11 11:20:27 -0600

    Initial Commit

---
+++ NightAndRadiance/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NightAndRadiance
+//
+//  Created by Darin Stoker on 3/11/18.
+//  Copyright © 2018 Darin Stoker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NightAndRadiance/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NightAndRadiance
+//
+//  Created by Darin Stoker on 3/11/18.
+//  Copyright © 2018 Darin Stoker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
