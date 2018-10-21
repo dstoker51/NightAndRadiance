9397a21887cac1306c8d3ce20bce0e0bf428bbaf
Darin Stoker <dstoker51@gmail.com>
2018-10-21 11:36:29 -0600

    Implemented optional failure initializer setup.

--- NightAndRadianceTests/NightAndRadianceTests.swift
+++ NightAndRadianceTests/NightAndRadianceTests.swift
@@ -21,85 +21,93 @@ class NightAndRadianceTests: XCTestCase {
         super.tearDown()
     }
     
-    // INITIALIZER TESTS
+    //----------------------------------------------------------------------------------------------------------------------------------
+    // MARK: - Initializer Tests
+    //----------------------------------------------------------------------------------------------------------------------------------
+
     func testCreateXYZ() {
         let vector = Vector(x: 1.25, y: 2.25, z: 3.25)
-        XCTAssert(vector.x == 1.25, "X doesn't equal 1.25")
-        XCTAssert(vector.y == 2.25, "Y doesn't equal 2.25")
-        XCTAssert(vector.z == 3.25, "Z doesn't equal 3.25")
+        XCTAssert(vector?.x == 1.25, "X doesn't equal 1.25")
+        XCTAssert(vector?.y == 2.25, "Y doesn't equal 2.25")
+        XCTAssert(vector?.z == 3.25, "Z doesn't equal 3.25")
     }
     
     func testCreateXYZWithZeros() {
         let vector = Vector(x: 0.0, y: 0.0, z: 0.0)
-        XCTAssert(vector.x == 0.0, "X doesn't equal 0.0")
-        XCTAssert(vector.y == 0.0, "Y doesn't equal 0.0")
-        XCTAssert(vector.z == 0.0, "Z doesn't equal 0.0")
+        XCTAssert(vector?.x == 0.0, "X doesn't equal 0.0")
+        XCTAssert(vector?.y == 0.0, "Y doesn't equal 0.0")
+        XCTAssert(vector?.z == 0.0, "Z doesn't equal 0.0")
     }
     
     func testCreateRThetas() {
-        let vector = Vector(r: 2, theta1: Double.pi/4.0, theta2: Double.pi/4.0)
+        let vector = Vector(r: 2, theta1: Double.pi/4.0, theta2: Double.pi/4.0)!
         XCTAssertEqual(vector.x, sqrt(2), accuracy: 0.000000001, "X should equal \(sqrt(2)), actually is \(vector.x)")
         XCTAssertEqual(vector.y, sqrt(2), accuracy: 0.000000001, "Y should equal \(sqrt(2)), actually is \(vector.y)")
         XCTAssertEqual(vector.z, sqrt(2), accuracy: 0.000000001, "Z should equal \(sqrt(2)), actually is \(vector.z)")
     }
     
     func testCreateRThetasWithZeroRadius() {
-        let vector = Vector(r: 0.0, theta1: Double.pi/4.0, theta2: Double.pi/4.0)
+        let vector = Vector(r: 0.0, theta1: Double.pi/4.0, theta2: Double.pi/4.0)!
         XCTAssertEqual(vector.x, 0.0, accuracy: 0.000000001, "X should equal 0.0, actually is \(vector.x)")
         XCTAssertEqual(vector.y, 0.0, accuracy: 0.000000001, "Y should equal 0.0, actually is \(vector.y)")
         XCTAssertEqual(vector.z, 0.0, accuracy: 0.000000001, "Z should equal 0.0, actually is \(vector.z)")
     }
     
     func testCreateRThetasWithZeroThetas() {
-        let vector = Vector(r: 2.0, theta1: 0.0, theta2: 0.0)
+        let vector = Vector(r: 2.0, theta1: 0.0, theta2: 0.0)!
         XCTAssertEqual(vector.x, 2.0, accuracy: 0.000000001, "X should equal 2.0, actually is \(vector.x)")
         XCTAssertEqual(vector.y, 0.0, accuracy: 0.000000001, "Y should equal 0.0, actually is \(vector.y)")
         XCTAssertEqual(vector.z, 0.0, accuracy: 0.000000001, "Z should equal 0.0, actually is \(vector.z)")
     }
     
     func testCreateRThetasWithAllZeros() {
-        let vector = Vector(r: 0.0, theta1: 0.0, theta2: 0.0)
+        let vector = Vector(r: 0.0, theta1: 0.0, theta2: 0.0)!
         XCTAssertEqual(vector.x, 0.0, accuracy: 0.000000001, "X should equal 0.0, actually is \(vector.x)")
         XCTAssertEqual(vector.y, 0.0, accuracy: 0.000000001, "Y should equal 0.0, actually is \(vector.y)")
         XCTAssertEqual(vector.z, 0.0, accuracy: 0.000000001, "Z should equal 0.0, actually is \(vector.z)")
     }
     
-    // METHOD TESTS
+    //----------------------------------------------------------------------------------------------------------------------------------
+    // MARK: - Method Tests
+    //----------------------------------------------------------------------------------------------------------------------------------
+
     func testDotProductBasic() {
-        let vector1 = Vector(x: 1.0, y: 1.0, z: 1.0)
-        let vector2 = Vector(x: 0.0, y: 0.0, z: 0.0)
+        let vector1 = Vector(x: 1.0, y: 1.0, z: 1.0)!
+        let vector2 = Vector(x: 0.0, y: 0.0, z: 0.0)!
         let scalarDot = vector1.dot(vector2)
         XCTAssertEqual(scalarDot, 0.0, "Dot product failure.")
     }
     
     func testDotProductBasicSameVector() {
-        let vector = Vector(x: 1.0, y: 1.0, z: 1.0)
+        let vector = Vector(x: 1.0, y: 1.0, z: 1.0)!
         let scalarDot = vector.dot(vector)
         XCTAssertEqual(scalarDot, 3.0, "Dot product failure.")
     }
     
     func testDotProductBothNegative() {
-        let vector1 = Vector(x: -1.0, y: -1.0, z: -1.0)
-        let vector2 = Vector(x: -1.0, y: -1.0, z: -1.0)
+        let vector1 = Vector(x: -1.0, y: -1.0, z: -1.0)!
+        let vector2 = Vector(x: -1.0, y: -1.0, z: -1.0)!
         let scalarDot = vector1.dot(vector2)    // Don't want to use vector equals yet since it is untested.
         XCTAssertEqual(scalarDot, 3.0, "Dot product failure.")
     }
     
     func testDotProductNegativeAndPositive() {
-        let vector1 = Vector(x: -1.0, y: -1.0, z: -1.0)
-        let vector2 = Vector(x: 1.0, y: 1.0, z: 1.0)
+        let vector1 = Vector(x: -1.0, y: -1.0, z: -1.0)!
+        let vector2 = Vector(x: 1.0, y: 1.0, z: 1.0)!
         let scalarDot = vector1.dot(vector2)    // Don't want to use vector equals yet since it is untested.
         XCTAssertEqual(scalarDot, -3.0, "Dot product failure.")
     }
     
-    // TIME TESTS
-    func timeTestOneMillionVectors() {
+    //----------------------------------------------------------------------------------------------------------------------------------
+    // MARK: - Time Tests
+    //----------------------------------------------------------------------------------------------------------------------------------
+    func testTimeToCreateOneMillionVectors() {
         self.measure {
             var vectors = [Vector]()
             for x in 0...99 {
                 for y in 0...99 {
                     for z in 0...99 {
-                        vectors.append(Vector(x: Double(x), y: Double(y), z: Double(z)))
+                        vectors.append(Vector(x: Double(x), y: Double(y), z: Double(z))!)
                     }
                 }
             }
